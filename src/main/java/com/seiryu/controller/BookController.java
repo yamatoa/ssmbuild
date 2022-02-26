ac7174f0bab2cdaa3cf961b39cffc83f06633363
yamatoa <kuresaitatu@icloud.com>
2022-02-26 20:00:51 +0900

    no message

---
+++ src/main/java/com/seiryu/controller/BookController.java
@@ -0,0 +1,68 @@
+package com.seiryu.controller;
+
+import com.seiryu.pojo.Books;
+import com.seiryu.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/book")
+public class BookController {
+
+    @Autowired
+    @Qualifier("BookServiceImpl")
+    private BookService bookService;
+
+    @RequestMapping("/allBook")
+    public String list(Model model) {
+        List<Books> list = bookService.queryAllBook();
+        model.addAttribute("list", list);
+        return "allBook";
+    }
+
+    //跳转到增加书籍页面
+    @RequestMapping("/toAddBook")
+    public String toAddPaper(){
+        return "addBook";
+    }
+
+    //添加书籍的请求
+    @RequestMapping("/addBook")
+    public String addBook(Books books){
+        System.out.println("addBook=>"+books);
+        bookService.addBook(books);
+        return "redirect:/book/allBook";
+    }
+
+    //跳转到修改书籍页面
+    @RequestMapping("/toUpdate")
+    public String toUpdatePaper(int id,Model model){
+        Books books = bookService.queryBookById(id);
+        model.addAttribute("QBook",books);
+        return  "updateBook";
+    }
+
+    //修改书籍
+    @RequestMapping("/updateBook")
+    public String updateBook(Books books){
+        System.out.println("updateBook=>"+books);
+        int i = bookService.updateBook(books);
+        if (i>0){
+            System.out.println("添加Books成功"+books);
+        }
+        return "redirect:/book/allBook";
+    }
+
+    //删除书籍
+    @RequestMapping("/deleteBook/{bookId}")
+    public String deleteBooks(@PathVariable("bookId") int id){
+        bookService.deleteBookById(id);
+        return  "redirect:/book/allBook";
+    }
+}
